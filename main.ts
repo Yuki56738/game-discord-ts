2206a9e9c9e72b66d8f70393357720f75c76862a
Yuki <github@risaton.net>
2024-12-11 21:45:51 +0900

    Set up project with initial TypeScript environment
    Added `main.ts` to load environment variables and configure Discord token and guild ID. Introduced `tsconfig.json` for TypeScript configuration and initialized `package-lock.json` with necessary dependencies including `dotenv`, `discord.js`, and `typescript`.

--- main.ts
+++ main.ts
@@ -1,7 +1,15 @@
 import dotenv from 'dotenv';
+import {Client, GatewayIntentBits, IntentsBitField} from "discord.js";
+let discord = require('discord.js');
 
 dotenv.config();
 
 const TOKEN = process.env.DISCORD_TOKEN;
 const TEST_GUILD_ID = process.env.GUILD_ID;
 
+const client = new Client({intents: 7796 });
+
+client.on('ready', e => {
+    console.log(`Logged in as ${client.user?.tag}!`);
+})
+client.login(TOKEN);
