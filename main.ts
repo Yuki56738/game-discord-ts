d4f1439e234505eebc2da4e1f236d405fe12e281
Yuki <github@risaton.net>
2024-12-11 22:24:18 +0900

    Set up project with initial TypeScript environment
    Added `main.ts` to load environment variables and configure Discord token and guild ID. Introduced `tsconfig.json` for TypeScript configuration and initialized `package-lock.json` with necessary dependencies including `dotenv`, `discord.js`, and `typescript`.

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 import dotenv from 'dotenv';
-import {Client, Collection, SlashCommandBuilder} from "discord.js";
+import {Client, Collection, Guild, SlashCommandBuilder} from "discord.js";
 
 dotenv.config();
 
@@ -8,15 +8,29 @@ const TEST_GUILD_ID = process.env.GUILD_ID;
 
 const client = new Client({intents: 7796 });
 
-client.on('ready', e => {
+client.on('ready', async () => {
     console.log(`Logged in as ${client.user?.tag}!`);
-    let commands = [];
-    const comandPing = new SlashCommandBuilder()
+    if (!client.application) {
+        console.error("Client application is not available.");
+        return;
+    }
+
+    const commands = [];
+    const commandPing = new SlashCommandBuilder()
         .setName('ping')
         .setDescription('Replies with Pong!');
-    commands.push(comandPing.toJSON());
-    client.application?.commands.set(commands);
-})
+    commands.push(commandPing.toJSON());
+
+    const guild = client.guilds.cache.get(TEST_GUILD_ID!); // 特定のギルドIDを指定
+    if (guild) {
+        await guild.commands.set(commands);
+        console.log("Slash commands have been set.");
+    } else {
+        console.error("Guild not found.");
+    }
+});
+
+
 
 
 
